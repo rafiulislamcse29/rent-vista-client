5850e1259e7c007464bae5329e105788897c340d
rafiulislamcse29 <rafiulislamcse29@gmail.com>
2024-08-10 22:57:14 -0700

    update code

--- js/baseUrl.js
+++ js/baseUrl.js
@@ -1,3 +1,3 @@
-// export const  BASE_URL='https://rent-vista-7tlr.onrender.com'
-export const BASE_URL = "http://127.0.0.1:8000"
+export const  BASE_URL='https://rent-vista-7tlr.onrender.com'
+// export const BASE_URL = "http://127.0.0.1:8000"
 
