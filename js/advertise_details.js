98bf574eacda1667c1552ac52892d03acbd04996
rafiulislamcse29 <rafiulislamcse29@gmail.com>
2024-08-13 09:05:43 -0700

    rent advertisement ui fix

--- js/advertise_details.js
+++ js/advertise_details.js
@@ -32,40 +32,44 @@ const displayAdvertisementsDetails = (advertise) => {
 
   const parentEl = document.getElementById('advertise-details')
   const div = document.createElement('div')
-  div.classList.add('row','g-0','g-lg-5')
+  div.classList.add('row','g-0','g-lg-3')
   fetch(`${BASE_URL}/category/list/${advertise.category}/`)
     .then(res => res.json())
     .then(category => {
       div.innerHTML = `
-                       <div class="col-md-6 overflow-hidden ">
-                            <img class='h-100 w-100 object-fit-cover' src='${advertise.image}' alt='${advertise.title}'>
-                        </div>
-                        <div class="col-md-6 d-flex flex-column justify-content-center p-3 p-lg-0">
-                            <h2 class="text-info">${advertise.title}</h2>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <p><span>Rent Category:</span> <span class="text-info">${category.name}</span></p>
-                                    <p><span>Rent Amount:</span> <span class="text-primary">${advertise.price}৳</span></p>
-                                    <p><span>Bedrooms:</span> <span class="text-info">${advertise.bedrooms}</span></p>
-                                </div>
-                                <div class="col-md-6">
-                                    <p><span>Location:</span> <span class="text-info">${advertise.location}</span></p>
-                                    <p><span>Amenities:</span> <span class="text-info">${advertise.amenities}</span></p>
-                                </div>
-                            </div>
-                            <p>${advertise.description}</p>
-                            <div class="d-flex gap-2">
-                                <div>
-                                    ${advertise.request_accepted ? ` <buttton  class='btn btn-success'>Rent Booked Already</button>` : ` <buttton onclick="handleRequestRent()"  class='btn btn-outline-primary'>Rent Request</button>`
-                                    }
-                                </div>
-                                 <div>
-                              <buttton onclick="handleFavouriteRent()" class='btn btn-outline-primary d-flex justify-content-center align-items-center gap-1'> 
-                              <span>Favourite Rent</span>
-                            <ion-icon name="heart-outline"></ion-icon></button>
-                            </div>
-                            </div>
-                        </div>
+    <div class="col-md-6 overflow-hidden">
+        <img class='h-100 w-100 object-fit-cover' src='${advertise.image}' alt='${advertise.title}'>
+    </div>
+    <div class="col-md-6 p-3 d-flex flex-column justify-content-center gap-2">
+      <div class='p-lg-3 p-0'>
+       <h3 class="text-info">${advertise.title}</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <p><span>Rent Category:</span> <span class="text-info">${category.name}</span></p>
+                <p><span>Rent Amount:</span> <span class="text-primary">${advertise.price}৳</span></p>
+                <p><span>Bedrooms:</span> <span class="text-info">${advertise.bedrooms}</span></p>
+            </div>
+            <div class="col-md-6">
+                <p><span>Location:</span> <span class="text-info">${advertise.location}</span></p>
+                <p><span>Amenities:</span> <span class="text-info">${advertise.amenities}</span></p>
+            </div>
+        </div>
+        <p class=''>${advertise.description}</p>
+        <div class="d-flex gap-2">
+            <div>
+                ${advertise.request_accepted ? ` <buttton class='btn btn-success'>Rent Booked Already</button>` : `
+                    <buttton onclick="handleRequestRent()" class='btn btn-outline-primary'>Rent Request</button>`
+                        }
+            </div>
+            <div>
+                <buttton onclick="handleFavouriteRent()"
+                    class='btn btn-outline-primary d-flex justify-content-center align-items-center gap-1'>
+                    <span>Favourite Rent</span>
+                    <ion-icon name="heart-outline"></ion-icon></button>
+            </div>
+        </div>
+      </div>
+    </div>
 
     `
       parentEl.appendChild(div)
